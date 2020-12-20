2f7b465dccc613bc6bb3924a0293fc28c2f4680b
cleophas <cleophasmashiri@gmail.com>
2020-12-20 10:23:50 +0200

    added tests

--- src/test/java/com/myspace/jbpm_elements_katas/ExclusiveGatewayPizzaDeliveryTest.java
+++ src/test/java/com/myspace/jbpm_elements_katas/ExclusiveGatewayPizzaDeliveryTest.java
@@ -18,33 +18,61 @@ import org.kie.internal.runtime.manager.context.EmptyContext;
 public class ExclusiveGatewayPizzaDeliveryTest extends JbpmJUnitBaseTestCase {
 
     private String processId = "jbpm-elements-katas.ExclusiveGatewayPizzaDelivery";
+    private RuntimeManager rtm;
+    private RuntimeEngine engine;
+    private KieSession kieSession;
+
+    public ExclusiveGatewayPizzaDeliveryTest() {
+        super();
+    }
 
     @Before
     public void setup() { 
-
+        rtm = createRuntimeManager("com/myspace/jbpm_elements_katas/ExclusiveGatewayPizzaDelivery.bpmn");
+        engine = getRuntimeEngine(EmptyContext.get());
+        kieSession = engine.getKieSession();
     }
 
     @Test
     public void givenCollectPizzaShouldFollowCollect() {
-        RuntimeManager rtm = createRuntimeManager("com/myspace/jbpm_elements_katas/ExclusiveGatewayPizzaDelivery.bpmn");
-        RuntimeEngine engine = getRuntimeEngine(EmptyContext.get());
-        KieSession kieSession = engine.getKieSession();
-
         assertNotNull(kieSession);
-
         Map<String, Object> params = new HashMap<String, Object>();
+        Order order = createOrder("collect");
+        params.put("order", order);
+        ProcessInstance pi = kieSession.startProcess(processId, params);
+        assertNotNull(pi);
+        assertEquals("Collected", order.getActions().get(0));
+    }
 
+    @Test
+    public void givenHomeDeliveryPizzaShouldFollowHomeDelivery() {
+        assertNotNull(kieSession);
+        Map<String, Object> params = new HashMap<String, Object>();
+        Order order = createOrder("homeDelivery");
+        params.put("order", order);
+        ProcessInstance pi = kieSession.startProcess(processId, params);
+        assertNotNull(pi);
+        assertEquals("Home Delivered", order.getActions().get(0));
+    }
+
+    @Test
+    public void givenNoDeliveySelectedPizzaShouldFollowCreateVoucher() {
+        assertNotNull(kieSession);
+        Map<String, Object> params = new HashMap<String, Object>();
+        Order order = createOrder(null);
+        params.put("order", order);
+        ProcessInstance pi = kieSession.startProcess(processId, params);
+        assertNotNull(pi);
+        assertEquals("Voucher Created", order.getActions().get(0));
+    }
+
+    private Order createOrder(String deliveryMethod) {
         Order order = new Order();
         order.setDelivery(new Delivery());
         order.getDelivery().setDelivered(true);
-        order.getDelivery().setDeliveryMethod("collect");
-        params.put("order", order);
-        ProcessInstance pi = kieSession.startProcess(processId, params);
-
-        assertNotNull(pi);
-
-        assertEquals(1, 1);
-
+        if (deliveryMethod!=null)
+            order.getDelivery().setDeliveryMethod(deliveryMethod);
+        return order;
     }
     
 }
